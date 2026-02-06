4ae67b17d9e699872f6f211aa21923f4ce2998c5
tonami-creator <elfriedtonami@gmail.com>
2026-02-06 13:09:24 +0100

    Lastnsia11

--- src/routes/inscription.routes.js
+++ src/routes/inscription.routes.js
@@ -1,8 +1,12 @@
-
 const router = require("express").Router();
 const inscriptionController = require("../controllers/inscription.controller");
 
-// Route pour l'inscription
+// Cette ligne répondra au navigateur si on tape l'URL à la main
+router.get("/", (req, res) => {
+    res.send("Le serveur d'inscription est prêt, mais vous devez envoyer un formulaire (POST) !");
+});
+
+// Ta route actuelle qui reçoit les données du formulaire
 router.post("/", inscriptionController.register);
 
 module.exports = router;
